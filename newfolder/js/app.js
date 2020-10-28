4e1abfb307f0346d3c97ce01500a45e6bd5cb078
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-10-29 00:55:53 +0530

    finished

--- newfolder/js/app.js
+++ newfolder/js/app.js
@@ -7,32 +7,13 @@ const wingButtons = document.querySelector('.wing-buttons');
 const wrapper = document.querySelector(".wrapperouter");
 const backButton = document.querySelector(".back-btn");
 const indexexplore = document.querySelector(".index__explore");
-// const indexarrow = document.querySelector(".index__arrow");
 const indexarrowicon = document.querySelector(".index__arrowicon");
+const indexwrapper = document.querySelector(".index__wrapper");
 const wingbuttons = document.querySelector(".wing-buttons");
 const tl = new TimelineMax();
-// tl
-//     // .fromTo(wrapper,.7, { x: '-100%' }, { x: '0%', ease: Power2.easeInOut },)
-//     .fromTo(
-//         container,
-//         .8,
-//         { height: "0vh" },
-//         { height: "100vh", ease: Power2.easeInOut },
-//     )
 
-//     .fromTo(
-//         container,
-//         .5,
-//         { boxShadow: " rgba(61,75,82,0.5) 0px 0px 30px 10px" },
-//         { boxShadow: " rgba(50,54,60,0.5) 0px 0px 30px 10px", ease: Power2.easeOut }
-//     )
-//     .fromTo(
-//         container,
-//         .5,
-//         { backgroundImage: 'linear-gradient(60deg, #29323c 0%, #485563 100%)' },
-//         { backgroundImage: 'linear-gradient(60deg, #29323c 0%, #485563 100%)', ease: Power2.easeInOut },
-//         "-=1"
-//     )
+indexexplore.style.borderRadius = (`${indexexplore.offsetHeight / 2}px`);
+
 indexexplore.addEventListener('click', () => {
     tl
         .fromTo(
@@ -41,13 +22,6 @@ indexexplore.addEventListener('click', () => {
             { left: "-100%" },
             { left: "0", ease: Power2.easeInOut }
         )
-        // .fromTo(
-        //     hero,
-        //     .1,
-        //     { transform: "translateX(0)" },
-        //     { transform: "translateX(-200%)", ease: Power2.easeInOut },
-        //     // "-=1.2"
-        // )
         .fromTo(
             yellow,
             1,
@@ -78,23 +52,10 @@ indexexplore.addEventListener('click', () => {
 indexarrowicon.addEventListener('click', () => {
     tl
         .fromTo(
-            yellow,
+            ml,
             1,
-            { left: "-100%" },
-            { left: "0", ease: Power2.easeInOut }
-        )
-        // .fromTo(
-        //     hero,
-        //     .1,
-        //     { transform: "translateX(0)" },
-        //     { transform: "translateX(-200%)", ease: Power2.easeInOut },
-        //     // "-=1.2"
-        // )
-        .fromTo(
-            yellow,
-            1,
-            { left: "0" },
-            { left: "-100%", ease: Power2.easeInOut },
+            { left: "100%" },
+            { left: "0%", ease: Power2.easeInOut }
         )
         .fromTo(
             wingButtons,
@@ -110,12 +71,7 @@ indexarrowicon.addEventListener('click', () => {
             { opacity: "0" },
             { opacity: "1", ease: Power2.easeInOut },
         )
-        .fromTo(
-            ml,
-            1,
-            { left: "100%" },
-            { left: "0%", ease: Power2.easeInOut }, "-=1.2"
-        )
+       
 })
 
 backButton.addEventListener('click', () => {
@@ -141,25 +97,4 @@ backButton.addEventListener('click', () => {
             { left: "100%", ease: Power2.easeInOut },
 
         )
-        .fromTo(
-            yellow,
-            1,
-            { left: "-100" },
-            { left: "0%", ease: Power2.easeInOut },
-            "-=1.2"
-        )
-        // .fromTo(
-        //     hero,
-        //     .1,
-        //     { transform: "translateX(-200%)" },
-        //     { transform: "translateX(0%)", ease: Power2.easeInOut },
-        //     "-=1.2"
-        // )
-        .fromTo(
-            yellow,
-            1,
-            { left: "0%" },
-            { left: "-100%", ease: Power2.easeInOut }
-        )
 })
-indexexplore.style.borderRadius = (`${indexexplore.offsetHeight / 2}px`)
