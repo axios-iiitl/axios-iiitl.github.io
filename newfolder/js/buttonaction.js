6eaacac1d2c100f42231b2317d2c36c971fe2cb1
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-10-29 18:18:10 +0530

    images optimised

--- newfolder/js/buttonaction.js
+++ newfolder/js/buttonaction.js
@@ -10,40 +10,46 @@ const wingName = document.querySelector('#wing-name');
 const wingQuote = document.querySelector('#wing-quote');
 const background = document.querySelector('.backdrop')
 const webContainer = document.querySelector('.webcontainer')
-
+const btn = document.querySelector('.btn')
+var urlofpage = "./wings/cyber.html";
 const details = {
     De: {
         text: "Design is an art. Design can be aesthetics. Design is so simple, that's why its so complicated.",
         img: 'url(./images/design1.jpg)',
         back: 'url(./images/design1.jpg)',
+        url: './wings/design.html',
     },
     We: {
         text: "Web development is not just about creating pretty layouts. It’s about understanding the marketing challenge behind your business.”",
-        img:  'url(./images/webd3.jpg)',
-        back:'url(./images/webd3.jpg)',
+        img: 'url(./images/webd3.jpg)',
+        back: 'url(./images/webd3.jpg)',
+        url: './wings/webd.html',
     },
     Ap: {
         text: 'It’s hard enough to find an error in your code when you’re looking for it; its even harder when you’ve assumed that your code is ERROR-FREE.',
-        img:  'url(./images/appd2.jpg)',
-        back:'url(./images/appd2.jpg)',
+        img: 'url(./images/appd2.jpg)',
+        back: 'url(./images/appd2.jpg)',
+        url: './wings/appd.html',
     },
     Ma: {
         text: 'A baby learns to crawl, walk and then run. We are in the crawling stage when it comes to applying machine learning.',
-        img:  'url(./images/machinebackground.jpeg)',
-        back:'url(./images/mlback.png)',
+        img: 'url(./images/machinebackground.jpeg)',
+        back: 'url(./images/mlback.png)',
+        url: './wings/ml.html',
     },
     Co: {
         text: 'A computer is a stupid machine with the ability to do incredibly smart things, while computer programmers are smart people with the ability to do incredibly stupid things. They are, in short, a perfect match.',
         img: 'url(./images/ccbackground.jpg)',
-        back:'url(./images/ccbackground.jpg)',
+        back: 'url(./images/ccbackground.jpg)',
+        url: './wings/cc.html',
     },
     Cy: {
         text: "Security used to be an inconvenience sometimes, but now it's a necessity all the time.",
-        img:  'url(./images/hacker.jpg)',
-        back:'url(./images/cyback.png)',
+        img: 'url(./images/hacker.jpg)',
+        back: 'url(./images/cyback.png)',
+        url: './wings/cyber.html',
     },
 }
-// const tl = new TimelineMax();
 designbutton.addEventListener('click', () => {
     bottomcoveranimation(designbutton)
 })
@@ -54,13 +60,13 @@ cyberbutton.addEventListener('click', () => {
     bottomcoveranimation(cyberbutton)
 })
 mlbutton.addEventListener('click', () => {
- rightcoveranimation(mlbutton)
+    rightcoveranimation(mlbutton)
 })
 webdbutton.addEventListener('click', () => {
- topcoveranimation(webdbutton)
+    topcoveranimation(webdbutton)
 })
 appdbutton.addEventListener('click', () => {
- leftcoveranimation(appdbutton)
+    leftcoveranimation(appdbutton)
 })
 
 function bottomcoveranimation(action) {
@@ -84,6 +90,7 @@ function bottomcoveranimation(action) {
         webContainer.style.background = details[wingg].img
         webContainer.style.backgroundSize = "cover";
         background.style.backgroundImage = details[wingg].back;
+        urlofpage = details[wingg].url;
     }, 1000)
 
 
@@ -109,6 +116,7 @@ function rightcoveranimation(action) {
         webContainer.style.background = details[wingg].img
         webContainer.style.backgroundSize = "cover"
         background.style.backgroundImage = details[wingg].back;
+        urlofpage = details[wingg].url;
     }, 1000)
 }
 function topcoveranimation(action) {
@@ -133,6 +141,7 @@ function topcoveranimation(action) {
         webContainer.style.background = details[wingg].img
         webContainer.style.backgroundSize = "cover"
         background.style.backgroundImage = details[wingg].back;
+        urlofpage = details[wingg].url;
     }, 1000)
 }
 
@@ -157,5 +166,10 @@ function leftcoveranimation(action) {
         webContainer.style.background = details[wingg].img
         webContainer.style.backgroundSize = "cover"
         background.style.backgroundImage = details[wingg].back;
+        urlofpage = details[wingg].url;
     }, 1000)
 }
+
+btn.addEventListener('click', () => {
+    location.assign(urlofpage);
+})
